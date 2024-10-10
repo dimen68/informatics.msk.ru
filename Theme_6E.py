38d31b6a9e67af01dd6fc60340f4b2e4a7721801
bochar68 <bochar.dg@gmail.com>
2024-10-10 15:04:25 +0300

    выведите индекс её первого и последнего появления

---
+++ Theme_6E.py
@@ -0,0 +1,8 @@
+# Первое и последнее вхождение
+stroka = input('Введите слово: ')
+first_in =  stroka.find('f')
+if first_in != -1:
+    print(first_in)
+last_in =  stroka.rfind('f',first_in+1)
+if last_in != -1:
+    print(last_in)
