df32aad7a039ba0dce5517ca8a0106b516b789bd
bochar68 <bochar.dg@gmail.com>
2024-10-14 13:44:31 +0300

    Удалите из строки все символы, чьи индексы делятся на 3

--- Theme_6N.py
+++ Theme_6N.py
@@ -1,8 +1,10 @@
 # Удалить каждый третий символ
 str_ = input('Введите строку: ')
+print(id(str_))
 str_new = ''
 for i in range(0, len(str_)):
     if i % 3 != 0:
         str_new += str_[i]
 str_ = str_new
+print(id(str_))
 print(str_)
